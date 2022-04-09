7345fa6dea89c104ccd82c0150ae15b3a627ab60
Ritesh6997 <96717182+Ritesh6997@users.noreply.github.com>
2022-04-09 21:33:22 +0530

    upadated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const food="pizza";
+  const age=13;
+  const links=["Service","Project","About"]
+  const skills=["Andriod","Blckberry","iphone","Windows Phone"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="App">
+      
+      <div>
+      <h1>Mobile Operating System</h1>
+        {skills.map((ele)=>{
+          return <Points skill={ele}/>
+        })}
+      </div>
+      </div>
+      <div id="Navbar">
+        <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/modern-bakery-logo-design-template-e979c6db88d6772062e4090687c00b7e_screen.jpg?ts=1602149907"></img>
+        <div>
+          {links.map((ele)=>{
+            return <Link link={ele}></Link>
+          })}
+        </div>
+        <button>Contact</button> 
+      </div>
+      </>
   );
+}
+//components
+function Points({skill}){
+  console.log("Skills props");
+  return <li className='li_ele'>{skill}</li>
+}
+function Link(l){
+  console.log("component1");
+  return <span className='links'>{l.link}</span>
+  
 }
 
 export default App;
